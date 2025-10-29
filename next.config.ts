116d285c87307422c86d8cc6992730ed7c3d426e
4uphwang <4uphwang@gmail.com>
2025-10-30 03:59:30 +0900

    chore(svg): Configure webpack for SVGR, add related module declarations, and update tooling.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,58 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack(config, { isServer }) {
+        const fileLoaderRule = config.module.rules.find((rule: any) =>
+            rule.test?.test?.('.svg'),
+        )
+
+        if (fileLoaderRule && fileLoaderRule.test instanceof RegExp) {
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: [{
+                    loader: '@svgr/webpack',
+                    options: {
+                        svgo: false,
+                        dimensions: false,
+                    },
+                },],
+            },
+        )
+
+        return config;
+    },
+    compiler: {
+        styledComponents: true,
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: [
+                        {
+                            loader: '@svgr/webpack',
+                            options: {
+                                svgo: false,
+                                dimensions: false, // 이 부분을 명시적으로 추가해야 합니다.
+                            }
+                        }
+                    ],
+                    as: '*.js',
+                },
+            },
+        },
+    },
 };
 
 export default nextConfig;
