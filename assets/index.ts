cb9d321380f4d60f527437c730062bb346227d51
4uphwang <4uphwang@gmail.com>
2025-10-30 04:00:16 +0900

    feat(assets): Built a unified asset export system (images and SVGs) to centralize resource management.

---
+++ assets/index.ts
@@ -0,0 +1,45 @@
+// 아이콘 (SVG)
+import { default as AprIcon } from './icons/APR.svg';
+import { default as BackIcon } from './icons/Back.svg';
+import { default as DownArrowIcon } from './icons/DownArrow.svg';
+import { default as EarnIcon } from './icons/Earn.svg';
+import { default as HomeIcon } from './icons/Home.svg';
+import { default as LeftArrowIcon } from './icons/LeftArrow.svg';
+import { default as MarketIcon } from './icons/Market.svg';
+import { default as MenuIcon } from './icons/Menu.svg';
+import { default as PointIcon } from './icons/Point.svg';
+import { default as PortfolioIcon } from './icons/Portfolio.svg';
+import { default as ReloadIcon } from './icons/Reload.svg';
+import { default as RightArrowIcon } from './icons/RightArrow.svg';
+import { default as SearchIcon } from './icons/Search.svg';
+
+
+// 이미지
+import BtcImage from './image/BTC.png';
+import HeaderLogoImage from './image/HeaderLogo.png';
+import UsdcImage from './image/USDC.png';
+import UsdtImage from './image/USDT.png';
+
+
+export {
+    // Icons
+    AprIcon,
+    BackIcon,
+
+    // Images
+    BtcImage,
+    DownArrowIcon,
+    EarnIcon,
+    HeaderLogoImage,
+    HomeIcon,
+    LeftArrowIcon,
+    MarketIcon,
+    MenuIcon,
+    PointIcon,
+    PortfolioIcon,
+    ReloadIcon,
+    RightArrowIcon,
+    SearchIcon, UsdcImage,
+    UsdtImage
+};
+
